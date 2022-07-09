a071922e8bab9fb10de68c6f108a47f77303672f
solonnliu <solonnliu@tencent.com>
2022-07-09 11:32:09 +0800

    feat: 处理拖拽画布宽度不准问题

--- src/modules/Home/LayoutPanel/index.tsx
+++ src/modules/Home/LayoutPanel/index.tsx
@@ -1,14 +1,17 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useRef } from 'react';
 import styles from './index.module.scss';
 import GridLayout from 'react-grid-layout';
 import DroppableContainer from '@/components/DragAndDrop/DroppableContainer';
 import DragContainer from '@/components/DragAndDrop/DragContainer';
 import HomeContext from '@/context/HomeContext';
+import { useSize } from 'ahooks';
 
 interface LayoutPanelProps {}
 
 const LayoutPanel: React.FC<LayoutPanelProps> = () => {
   const { droppedComponents } = useContext(HomeContext);
+  const ref = useRef(null);
+  const layoutSize = useSize(ref);
 
   useEffect(() => {
     console.log('LayoutPanel LayoutPanel', droppedComponents);
@@ -19,26 +22,28 @@ const LayoutPanel: React.FC<LayoutPanelProps> = () => {
     { i: 'c', x: 4, y: 0, w: 1, h: 2 }
   ];
   return (
-    <DroppableContainer
-      className={styles.layoutPanel}
-      droppableId={'layout'}
-      type={'vertical'}
-      direction="horizontal"
-    >
-      <DragContainer draggableId={'3'} index={1} isDragDisabled={true}>
-        <GridLayout
-          className="layout"
-          layout={layout}
-          cols={12}
-          rowHeight={30}
-          width={1200}
-        >
-          <div key="a">a</div>
-          <div key="b">b</div>
-          <div key="c">c</div>
-        </GridLayout>
-      </DragContainer>
-    </DroppableContainer>
+    <div ref={ref} className={styles.layoutPanel}>
+      <DroppableContainer
+        droppableId={'layout'}
+        type={'vertical'}
+        direction="horizontal"
+      >
+        <DragContainer draggableId={'3'} index={1} isDragDisabled={true}>
+          <GridLayout
+            className="layout"
+            layout={layout}
+            cols={24}
+            rowHeight={10}
+            width={layoutSize?.width || 950}
+            isBounded={true}
+          >
+            <div key="a">a</div>
+            <div key="b">b</div>
+            <div key="c">c</div>
+          </GridLayout>
+        </DragContainer>
+      </DroppableContainer>
+    </div>
   );
 };
 
